1870983e8bd95dcba76cfccfe2ce31bc9c57476f
TomtomPudding <tomtomhobby1125@gmail.com>
2019-10-05 18:01:46 +0900

    New-Api init commit

---
+++ api/mysql/init/0001-crate-table.sql
@@ -0,0 +1,6 @@
+CREATE TABLE trabeldata(
+    id            INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    position      VARCHAR(100) NOT NULL,
+    content       VARCHAR(2000) NOT NULL,
+    usercd        INT(20)
+);
