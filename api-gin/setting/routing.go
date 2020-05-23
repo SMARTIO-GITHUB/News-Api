eaab2ed6c3d63210ceb2d9844d769996515e5405
TomtomPudding <tomtomhobby1125@gmail.com>
2020-05-24 06:00:56 +0900

    [add] cros対策

--- api-gin/setting/routing.go
+++ api-gin/setting/routing.go
@@ -2,6 +2,7 @@ package setting
 
 import (
     "github.com/gin-gonic/gin"
+    "github.com/gin-contrib/cors"
     "bot-api/controller"
     "bot-api/db"
 )
@@ -16,8 +17,15 @@ func NewRouting(db *db.DB) *Routing {
         DB: db,
         Gin: gin.Default(),
     }
+    r.setAccessFilterSetting()
     r.setRouting()
     return r
+}
+
+func (r *Routing) setAccessFilterSetting() {
+    config := cors.DefaultConfig()
+    config.AllowOrigins = []string{"http://localhost:8081", "http://localhost:8082"}
+    r.Gin.Use(cors.New(config))
 }
 
 func (r *Routing) setRouting() {
